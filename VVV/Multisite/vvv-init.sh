aaa4af97eb2472b05f4a049ff8f9ca22c7ecef1f
Luis Rodrigues <hello@goblindegook.net>
2015-02-23 00:45:07 +0000

    Indentation fixes

--- VVV/Multisite/vvv-init.sh
+++ VVV/Multisite/vvv-init.sh
@@ -10,15 +10,15 @@ ADMIN_EMAIL="engenharia@log.pt"
 THEME="{{THEME SLUG}}"
 
 NETWORK_PLUGINS=(
-    autoptimize
-    regenerate-thumbnails
-    wordpress-seo
+  autoptimize
+  regenerate-thumbnails
+  wordpress-seo
 )
 
 PLUGINS=(
-    advanced-custom-fields-pro
-    black-studio-tinymce-widget
-    gravityforms
+  advanced-custom-fields-pro
+  black-studio-tinymce-widget
+  gravityforms
 )
 
 ## PROVISIONING ##
@@ -29,20 +29,20 @@ composer update
 
 if ! $(wp core is-installed); then
 
-    echo " * Downloading WordPress"
+  echo " * Downloading WordPress"
 
-    wp core download
+  wp core download
 
-    echo " * Creating database schema ${DATABASE}"
+  echo " * Creating database schema ${DATABASE}"
 
-    mysql -u root --password=root -e "CREATE DATABASE IF NOT EXISTS ${DATABASE}"
-    mysql -u root --password=root -e "GRANT ALL PRIVILEGES ON ${DATABASE}.* TO wp@localhost IDENTIFIED BY 'wp';"
+  mysql -u root --password=root -e "CREATE DATABASE IF NOT EXISTS ${DATABASE}"
+  mysql -u root --password=root -e "GRANT ALL PRIVILEGES ON ${DATABASE}.* TO wp@localhost IDENTIFIED BY 'wp';"
 
-    echo " * Configuring WordPress"
+  echo " * Configuring WordPress"
 
-    WP_CACHE_KEY_SALT=`date +%s | sha256sum | head -c 64`
+  WP_CACHE_KEY_SALT=`date +%s | sha256sum | head -c 64`
 
-    wp core config --dbname="${DATABASE}" --dbuser=wp --dbpass=wp --extra-php <<PHP
+  wp core config --dbname="${DATABASE}" --dbuser=wp --dbpass=wp --extra-php <<PHP
 
 define( 'WP_DEBUG', true );
 define( 'WP_DEBUG_LOG', true );
@@ -59,29 +59,29 @@ define( 'WP_CACHE_KEY_SALT', '$WP_CACHE_KEY_SALT' );
 define( 'WP_ENV', 'development' );
 PHP
 
-    echo " * Setting up multisite \"$TITLE\" at $URL"
+  echo " * Setting up multisite \"$TITLE\" at $URL"
 
-    wp core multisite-install --url="$URL" --title="$TITLE" --admin_user=admin --admin_password=password --admin_email="$ADMIN_EMAIL"
+  wp core multisite-install --url="$URL" --title="$TITLE" --admin_user=admin --admin_password=password --admin_email="$ADMIN_EMAIL"
 
-    wp super-admin add admin
+  wp super-admin add admin
 
-    # Uncomment and edit to set up additional sites:
-    # wp site create --title="{{SUBSITE TITLE}}" --slug="{{SUBSITE SLUG}}"
+  # Uncomment and edit to set up additional sites:
+  # wp site create --title="{{SUBSITE TITLE}}" --slug="{{SUBSITE SLUG}}"
 
-    echo " * Configuring pretty permalinks"
+  echo " * Configuring pretty permalinks"
 
-    wp option update permalink_structure "/%postname%/"
+  wp option update permalink_structure "/%postname%/"
 
-    # Uncomment and edit to set up subsite permalinks:
-    # wp option update permalink_structure "/%postname%/" --url="http://$URL/{{SUBSITE SLUG}}/"
+  # Uncomment and edit to set up subsite permalinks:
+  # wp option update permalink_structure "/%postname%/" --url="http://$URL/{{SUBSITE SLUG}}/"
 
-    echo " * Importing test content"
+  echo " * Importing test content"
 
-    curl -OLs https://raw.githubusercontent.com/manovotny/wptest/master/wptest.xml
-    wp plugin install wordpress-importer
-    wp plugin activate wordpress-importer
-    wp import wptest.xml --authors=create
-    rm wptest.xml
+  curl -OLs https://raw.githubusercontent.com/manovotny/wptest/master/wptest.xml
+  wp plugin install wordpress-importer
+  wp plugin activate wordpress-importer
+  wp import wptest.xml --authors=create
+  rm wptest.xml
 
 	## OBJECT CACHE ##
 	
